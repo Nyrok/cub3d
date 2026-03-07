df939262b3b5dd558a302dcf3dbbe1f6af7b35c0
Hamza Konte <47859726+Nyrok@users.noreply.github.com>
2026-03-07 14:15:00 +0100

    feat: implement DDA raycasting engine in backtrack.c
    cast_rays: main loop over each screen column, picks N/S/E/W texture.
    init_ray: compute ray direction and delta distances from camera plane.
    calc_step: determine DDA step direction and initial side distances.
    perform_dda: traverse grid until hitting a wall cell.
    get_wall_info: compute perp distance, wall height, texture coordinates.

--- backtrack.c
+++ backtrack.c
@@ -10,98 +10,121 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./includes/so_long.h"
+#include "./includes/cub3d.h"
 
-static void	make_copy(t_map *map)
+static void	init_ray(t_cub *cub, t_ray *ray, int x)
 {
-	size_t	i;
+	double	cam_x;
 
-	i = 0;
-	map->copy_lines = ft_calloc(map->line_count + 1, sizeof(char *));
-	if (!map->copy_lines)
-		exit_error(map, "Malloc failed for copy lines.");
-	while (i < map->line_count)
+	cam_x = 2.0 * x / WIN_W - 1.0;
+	ray->dir_x = cub->player.dir_x + cub->player.plane_x * cam_x;
+	ray->dir_y = cub->player.dir_y + cub->player.plane_y * cam_x;
+	ray->map_x = (int)cub->player.pos_x;
+	ray->map_y = (int)cub->player.pos_y;
+	if (ray->dir_x == 0.0)
+		ray->delta_dist_x = 1e30;
+	else
+		ray->delta_dist_x = fabs(1.0 / ray->dir_x);
+	if (ray->dir_y == 0.0)
+		ray->delta_dist_y = 1e30;
+	else
+		ray->delta_dist_y = fabs(1.0 / ray->dir_y);
+}
+
+static void	calc_step(t_ray *ray, t_player *p)
+{
+	if (ray->dir_x < 0)
 	{
-		map->copy_lines[i] = ft_strdup(map->lines[i]);
-		i++;
+		ray->step_x = -1;
+		ray->side_dist_x = (p->pos_x - ray->map_x) * ray->delta_dist_x;
+	}
+	else
+	{
+		ray->step_x = 1;
+		ray->side_dist_x = (ray->map_x + 1.0 - p->pos_x) * ray->delta_dist_x;
+	}
+	if (ray->dir_y < 0)
+	{
+		ray->step_y = -1;
+		ray->side_dist_y = (p->pos_y - ray->map_y) * ray->delta_dist_y;
+	}
+	else
+	{
+		ray->step_y = 1;
+		ray->side_dist_y = (ray->map_y + 1.0 - p->pos_y) * ray->delta_dist_y;
 	}
 }
 
-static int	check_all_reachable(t_map *map, char c)
+static void	perform_dda(t_cub *cub, t_ray *ray)
 {
-	size_t	x;
-	size_t	y;
+	int	hit;
 
-	y = 0;
-	while (y < map->line_count)
+	hit = 0;
+	while (!hit)
 	{
-		x = 0;
-		while (x < map->line_len)
+		if (ray->side_dist_x < ray->side_dist_y)
 		{
-			if (map->copy_lines[y][x] == c)
-				return (0);
-			x++;
+			ray->side_dist_x += ray->delta_dist_x;
+			ray->map_x += ray->step_x;
+			ray->side = 0;
 		}
-		y++;
-	}
-	return (1);
-}
-
-static void	fill(t_map *map, int x, int y)
-{
-	int	line_count;
-	int	line_len;
-
-	line_count = (int)map->line_count;
-	line_len = (int)map->line_len;
-	if (x < 0 || y < 0 || y >= line_count || x >= line_len)
-		return ;
-	if (map->copy_lines[y][x] == WALL || map->copy_lines[y][x] == VISITED)
-		return ;
-	if (map->copy_lines[y][x] == EXIT && !check_all_reachable(map, ITEM))
-	{
-		map->copy_lines[y][x] = EMPTY;
-		return ;
-	}
-	map->copy_lines[y][x] = VISITED;
-	fill(map, x + 1, y);
-	fill(map, x - 1, y);
-	fill(map, x, y + 1);
-	fill(map, x, y - 1);
-}
-
-static int	find_spawn(t_map *map)
-{
-	size_t	x;
-	size_t	y;
-
-	y = 0;
-	while (y < map->line_count)
-	{
-		x = 0;
-		while (x < map->line_len)
+		else
 		{
-			if (map->copy_lines[y][x] == PLAYER)
-			{
-				map->player_x = (int)x;
-				map->player_y = (int)y;
-				return (1);
-			}
-			x++;
+			ray->side_dist_y += ray->delta_dist_y;
+			ray->map_y += ray->step_y;
+			ray->side = 1;
 		}
-		y++;
+		if (cub->map[ray->map_y][ray->map_x] == '1')
+			hit = 1;
 	}
-	return (0);
 }
 
-void	backtrack(t_map *map)
+static void	get_wall_info(t_ray *ray, t_player *p, t_img *tex)
 {
-	make_copy(map);
-	if (!find_spawn(map))
-		exit_error(map, "(Map) Spawn point not found.");
-	fill(map, map->player_x, map->player_y);
-	if (!check_all_reachable(map, ITEM))
-		exit_error(map, "(Map) Map not reachable.");
-	if (!check_all_reachable(map, EXIT))
-		exit_error(map, "(Map) Exit not reachable.");
+	if (ray->side == 0)
+		ray->perp_dist = ray->side_dist_x - ray->delta_dist_x;
+	else
+		ray->perp_dist = ray->side_dist_y - ray->delta_dist_y;
+	ray->line_height = (int)(WIN_H / ray->perp_dist);
+	ray->draw_start = WIN_H / 2 - ray->line_height / 2;
+	if (ray->draw_start < 0)
+		ray->draw_start = 0;
+	ray->draw_end = WIN_H / 2 + ray->line_height / 2;
+	if (ray->draw_end >= WIN_H)
+		ray->draw_end = WIN_H - 1;
+	if (ray->side == 0)
+		ray->wall_x = p->pos_y + ray->perp_dist * ray->dir_y;
+	else
+		ray->wall_x = p->pos_x + ray->perp_dist * ray->dir_x;
+	ray->wall_x -= floor(ray->wall_x);
+	ray->tex_x = (int)(ray->wall_x * tex->width);
+	if (ray->side == 0 && ray->dir_x > 0)
+		ray->tex_x = tex->width - ray->tex_x - 1;
+	if (ray->side == 1 && ray->dir_y < 0)
+		ray->tex_x = tex->width - ray->tex_x - 1;
+}
+
+void	cast_rays(t_cub *cub)
+{
+	t_ray	ray;
+	int		x;
+	int		tex_idx;
+
+	x = 0;
+	while (x < WIN_W)
+	{
+		init_ray(cub, &ray, x);
+		calc_step(&ray, &cub->player);
+		perform_dda(cub, &ray);
+		tex_idx = TEX_NO;
+		if (ray.side == 1 && ray.step_y > 0)
+			tex_idx = TEX_SO;
+		else if (ray.side == 0 && ray.step_x > 0)
+			tex_idx = TEX_EA;
+		else if (ray.side == 0 && ray.step_x < 0)
+			tex_idx = TEX_WE;
+		get_wall_info(&ray, &cub->player, &cub->tex[tex_idx]);
+		draw_column(cub, &ray, x);
+		x++;
+	}
 }
